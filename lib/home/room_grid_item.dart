03224aa486f15f739dbf5371370a81b4ab87a947
ahmedfouad311 <ahmedfouad30@hotmail.com>
2021-12-27 04:02:05 +0200

    add frid

---
+++ lib/home/room_grid_item.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, must_be_immutable, prefer_const_literals_to_create_immutables
+
+import 'package:chatapp/data/rooms.dart';
+import 'package:chatapp/general/utils.dart';
+import 'package:flutter/material.dart';
+
+class RoomGridItem extends StatelessWidget {
+  Rooms rooms;
+  RoomGridItem(this.rooms);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: Colors.white,
+        boxShadow: [BoxShadow(color: Colors.grey, offset: Offset(4, 4))],
+      ),
+      child: Column(
+        children: [
+          Image.asset(Category.fromId(rooms.categoryId).imagePath,
+              height: 80, width: double.infinity),
+          Text(
+            rooms.roomName,
+             maxLines: 1,
+             style: TextStyle(
+               color: Colors.black,
+               fontSize: 18,
+               fontWeight: FontWeight.bold
+             ),
+             ),
+        ],
+      ),
+    );
+  }
+}
